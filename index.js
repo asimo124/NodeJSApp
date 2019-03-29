b2c7c14e098374a7dab4078807f4a11ff4a6383f
asimo124 <asimo124@yahoo.com>
2019-03-29 13:09:19 -0500

    Testing 'createServer' code from Udemy NodeJS tutorial

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-
+/*/
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
@@ -11,3 +11,18 @@ express()
 	.get('/', (req, res) => res.render('pages/index'))
 	.get('/cool', (req, res) => res.send(cool()))
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//*/
+
+var http = require('http')
+var fs = require('fs')
+var console = require('console')
+const output = fs.createWriteStream('./stdout.log');
+const errorOutput = fs.createWriteStream('./stderr.log');
+const logger = new console.Console(output, errorOutput);
+
+http.createServer(function(req, res) {
+	logger.log("req: ", req.url);
+	res.writeHead(200, { 'Content-Type': 'text/plain' })
+	res.end('Hello world\n');
+}).listen(PORT, '127.0.0.1');
+//*/
