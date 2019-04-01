c54eadad047c5e82ce0469b0ba69b38e80f1b920
Alex Hawley <asimo124@yahoo.com>
2019-04-01 00:31:15 -0500

    Continuing with Express Tutorial. Working on NPM MySQL

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const cool = require('cool-ascii-faces')
+
+const bodyParser = require('body-parser')
+
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+var jsonParser = bodyParser.json();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'us-cdbr-iron-east-03.cleardb.net',
+	user     : '/bd5f7a78084af4',
+	password : 'be0208f5',
+	database : 'express2'
+});
+
+connection.connect();
+
+
 
 //*/
 express()
@@ -10,11 +30,29 @@ express()
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => res.render('pages/index'))
 	.get('/page', (req, res) => res.render('pages/page'))
+	.get('/login_page', (req, res) => res.render('pages/login_page'))
+	.post('/login', urlencodedParser, (req, res) => {
+		if (!req.body) req.sendStatus(400)
+		res.json({ id: req.body.username })
+	})
+	.post('/jsontest', jsonParser, (req, res) => {
+		res.json({
+			username: req.body.username
+		})
+	})
 	.get('/person/:id', (req, res) => {
-		res.json({ id: req.params.id })
-		//res.render('pages/page')
+		//res.json({ id: req.params.id })
+		res.render('pages/person', { fname: req.query.fname })
 	})
 	.get('/api', (req, res) => res.json({ test: 1234, data: "Here" }))
+	.get('/books/check', (req, res) => {
+		connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+			if (error) throw error;
+			console.log('The solution is: ', results[0].solution);
+			res.json({ status: results[0].solution})
+		});
+
+	})
 	.get('/cool', (req, res) => res.send(cool()))
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 /*/
